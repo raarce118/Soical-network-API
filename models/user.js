6800645e983b6cec3824c81913831151d4c80857
Rich Arce <raarce118@gmail.com>
2022-05-02 19:40:03 -0700

    added to user Model and index...need to work on server

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const { Schema } = require("mongoose");
+
+const { schema, model } = requre('mongoose');
+
+ // var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; <-- stack overflow Match and Validate property
+
+ // Used format from pizza hunt module. Username, email, thoughts, and friends required for this challenge.
+const UserSchema =  new Schema(
+{
+
+     userName: {
+         type: String,
+         required: true,
+         unique: true,
+         trim: true
+    },
+
+     email: {
+         type: String,
+         required: true,
+         unique: true,
+         validate: [validateEmail, 'Please use a valid email address'],
+         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+ // Similar to comments array from pizza hunt module. Thoughts will be coming from User
+     thoughts: [
+        {
+         type: schema.Types.ObjectId,
+         ref: 'Thought'
+        }
+    ],
+     friends: [
+         {
+         type: schema.Types.ObjectId,
+         ref: 'User'
+         }
+     ]
+},
+{
+    toJSON: {
+         virtuals: true,
+         getters: true
+    },
+ // prevents virtuals from creating duplicate of _id as `id`    
+         id: false
+}
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friend.length;
+  });
+  
+  const User = model('User', UserSchema);
+  
+  module.exports = User;
