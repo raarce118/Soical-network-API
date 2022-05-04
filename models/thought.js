e1ce7ed200f67c1adf99edb4c1838a68350bf21d
Rich Arce <raarce118@gmail.com>
2022-05-03 21:53:04 -0700

    reorganized thought model

--- models/thought.js
+++ models/thought.js
@@ -1,42 +1,5 @@
 const { Schema, model, Types } = require("mongoose");
 const { DateTime } = require("luxon")
-const ThoughtSchema = new Schema(
-
-    {
-
-thoughtText: {
-    type: String,
-    required: true,
-    minLength: [1, 'Needs to be between 1 and 280 characters!'],
-    maxLength: [2, 'Needs to be between 1 and 280 characters!'],
-    trim: true
-},
-
-createdAt: {
-    type: Date,
-    default: DateTime.now(),
-    get: createdAtVal => createdAtVal.toLocaleString()
-},
-
-username: {
-    type: String,
-    required: true,
-    trim: true
-},
-
-reactions: [ReactionsSchema]
-
-},
-{
-    toJSON: {
-         virtuals: true,
-         getters: true
-    },
-     id: false
-}
-);
-
-
 
 
 const ReactionsSchema = new Schema(
@@ -75,8 +38,45 @@ createdAt: {
     },
      id: false
 }
-
 );
+
+const ThoughtSchema = new Schema(
+
+    {
+
+thoughtText: {
+    type: String,
+    required: true,
+    minLength: [1, 'Needs to be between 1 and 280 characters!'],
+    maxLength: [2, 'Needs to be between 1 and 280 characters!'],
+    trim: true
+},
+
+createdAt: {
+    type: Date,
+    default: DateTime.now(),
+    get: createdAtVal => createdAtVal.toLocaleString()
+},
+
+username: {
+    type: String,
+    required: true,
+    trim: true
+},
+
+reactions: [ReactionsSchema]
+
+},
+{
+    toJSON: {
+         virtuals: true,
+         getters: true
+    },
+     id: false
+}
+);
+
+
 
 // get total count of comments and replies on retrieval
 ThoughtSchema.virtual('reactionCount').get(function() {
